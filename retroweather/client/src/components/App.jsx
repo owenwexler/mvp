70f872d075db4b1ac8b6ffb61bcbf7ac6a753d24
Owen Wexler <owenbwexler@gmail.com>
2020-10-31 19:14:00 -0400

    Add Hooks to client

--- retroweather/client/src/components/App.jsx
+++ retroweather/client/src/components/App.jsx
@@ -1,6 +1,15 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
+
 
 const App = () => {
+
+  const [location, setLocation] = useState('');
+  const [weatherData, setWeatherData] = useState({});
+
+  useEffect(() => {
+    console.log('N I N T E N D O  V I B E S');
+  }, []);
+
   return (
     <h1>WELCOME TO THE RETRODOME.  MEOW MEOW MEOW.</h1>
   )
