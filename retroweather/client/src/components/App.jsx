555380d45f6b2bac6509ad328959a7d42cbe72c6
Owen Wexler <owenbwexler@gmail.com>
2020-10-31 19:46:38 -0400

    Begin HelloWorlding GET and POST routes on server

--- retroweather/client/src/components/App.jsx
+++ retroweather/client/src/components/App.jsx
@@ -1,8 +1,6 @@
 import React, { useState, useEffect } from 'react';
 
-
 const App = () => {
-
   const [location, setLocation] = useState('');
   const [weatherData, setWeatherData] = useState({});
 
