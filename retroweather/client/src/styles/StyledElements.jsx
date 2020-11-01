28caf0bdb6de7a6ef1a8b0c16a3fc023348d358e
Owen Wexler <owenbwexler@gmail.com>
2020-11-01 13:53:08 -0500

    Refactor GlobalStyles to separate file

---
+++ retroweather/client/src/styles/StyledElements.jsx
@@ -0,0 +1,11 @@
+import styled, { createGlobalStyle } from 'styled-components';
+
+const GlobalStyles = createGlobalStyle`
+@import url('../../../font/press_start/PressStart2P-Regular.ttf');
+
+body {
+  font-family: 'Press Start 2P', sans-serif;
+}
+`
+
+export default GlobalStyles;
