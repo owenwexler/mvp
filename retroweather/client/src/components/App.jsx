95bc0c15f36bdf4428bc89b51f9357659e43f748
Owen Wexler <owenbwexler@gmail.com>
2020-10-31 20:39:02 -0400

    Add eight bit font to global styles

--- retroweather/client/src/components/App.jsx
+++ retroweather/client/src/components/App.jsx
@@ -1,15 +1,27 @@
 import React, { useState, useEffect } from 'react';
+import styled, { createGlobalStyle } from 'styled-components';
 
 const App = () => {
   const [location, setLocation] = useState('');
   const [weatherData, setWeatherData] = useState({});
+
+  const GlobalStyles = createGlobalStyle`
+    @import url('../../../font/press_start/PressStart2P-Regular.ttf');
+
+    body {
+      font-family: 'Press Start 2P', sans-serif;
+    }
+  `
 
   useEffect(() => {
     console.log('N I N T E N D O  V I B E S');
   }, []);
 
   return (
-    <h1>WELCOME TO THE RETRODOME.  MEOW MEOW MEOW.</h1>
+    <div>
+      <GlobalStyles />
+      <h1>WELCOME TO THE RETRODOME.  MEOW MEOW MEOW.</h1>
+    </div>
   )
 }
 
